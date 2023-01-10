17e31460bee56849cab4b96a188c2021fb8efacb
SmartPixel <pixelart@MacBook-Pro-SmartPixel.local>
2023-01-10 23:34:18 +0100

    feat/temporary_refactor_no_static

--- movieshelf/movieshelf/settings.py
+++ movieshelf/movieshelf/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == "1"
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 if not DEBUG:
