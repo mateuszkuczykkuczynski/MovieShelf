354efde693b770d0239a5bfeb9810dc9360267cd
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-01-15 22:58:22 +0100

    test/new_tests

--- members/tests.py
+++ members/tests.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Tests coming really soon...
+
+class SignUpPageTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testuser@gmail.com',
+            age=69)
+
+    def test_signup_page_status_code(self):
+        response = self.client.get('/members/sign_up/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('sign_up'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('sign_up'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_signup_form(self):
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.user.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.user.email)
+        self.assertEqual(get_user_model().objects.all()[0].age, self.user.age)
