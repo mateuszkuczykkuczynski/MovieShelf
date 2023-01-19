6247b984dee358aa89da8443e53c00d29be0e997
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-01-19 23:48:54 +0100

    feat/members_app_updates

--- members/models.py
+++ members/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(get_user_model(), null=True, on_delete=models.CASCADE, related_name='profiles')
+    avatar = models.ImageField(upload_to='media/', null=True, blank=True)
+    socials = models.CharField(max_length=200, null=True, blank=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
+    def get_absolute_url(self):
+        return reverse('user_profile', args=[str(self.id)])
+
+
--- members/views.py
+++ members/views.py
@@ -1,65 +1,69 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
-from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
-from scenario.models import Profile
-from .forms import ProfilePageForm
+from .models import UserProfile
 
 
-class PasswordsChangeView(PasswordChangeView):
-    model = Profile
-    form_class = PasswordChangeForm
-    success_url = reverse_lazy('password_success')
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+# from django.shortcuts import render, get_object_or_404
+# from django.contrib.auth.views import PasswordChangeView
+# from scenario.models import Profile
+# from .forms import ProfilePageForm
 
 
-class ProfileDetailsView(generic.UpdateView):
-    model = Profile
-    template_name = 'registration/profile_details.html'
-    fields = ['bio', 'profile_picture', 'social_media_link']
-    success_url = reverse_lazy('homepage')
-
-
-class ProfilePageView(generic.DetailView):
-    model = Profile
-    template_name = 'registration/user_profile.html'
-
-    def get_context_data(self, *args, **kwargs):
-        users = Profile.objects.all()
-        context = super(ProfilePageView, self).get_context_data(*args, **kwargs)
-
-        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
-
-        context["page_user"] = page_user
-        return context
-
-
-class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
-    template_name = 'registration/registration.html'
+class UserSignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
 
 
-class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
-    template_name = 'registration/edit_profile.html'
-    success_url = reverse_lazy('homepage')
-
-    def get_object(self):
-        return self.request.user
-
-
-class CreateProfilePageView(generic.CreateView):
-    model = Profile
-    form_class = ProfilePageForm
-    template_name = "registration/create_user_profile_page.html"
-
-    # fields = '__all__'
+class CreateProfileView(LoginRequiredMixin, CreateView):
+    model = UserProfile
+    template_name = "create_user_profile.html"
+    fields = ['avatar', 'socials', 'bio']
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
 
-def password_success(request):
-    return render(request, 'registration/password_success.html')
+class UpdateProfileView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = UserProfile
+    template_name = 'edit_user_profile.html'
+    fields = ['avatar', 'socials', 'bio']
+    success_url = reverse_lazy('user_profile_details')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
+
+
+class DeleteProfileView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = UserProfile
+    template_name = 'delete_user_profile.html'
+    success_url = reverse_lazy('user_profile_details')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
+
+
+class ProfileDetailsView(LoginRequiredMixin, DetailView):
+    model = UserProfile
+    template_name = 'user_profile_details.html'
+
+
+# class ProfilePageView(generic.DetailView):
+#     model = Profile
+#     template_name = 'registration/user_profile.html'
+#
+#     def get_context_data(self, *args, **kwargs):
+#         users = Profile.objects.all()
+#         context = super(ProfilePageView, self).get_context_data(*args, **kwargs)
+#
+#         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+#
+#         context["page_user"] = page_user
+#         return context
+
