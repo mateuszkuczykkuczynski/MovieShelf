8c867602f2b66c32940d01e7453839333ec7bba4
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-01-20 23:51:43 +0100

    test/members_app_tests_update

--- members/tests.py
+++ members/tests.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.test.client import Client
+
+from .models import UserProfile
 
 
 class SignUpPageTest(TestCase):
@@ -29,3 +32,42 @@ class SignUpPageTest(TestCase):
         self.assertEqual(get_user_model().objects.all()[0].username, self.user.username)
         self.assertEqual(get_user_model().objects.all()[0].email, self.user.email)
         self.assertEqual(get_user_model().objects.all()[0].age, self.user.age)
+
+
+class UserProfileTests(TestCase):
+
+    def setUp(self):
+
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testuser@gmail.com',
+            age=420,
+            password='Letmeout5!'
+        )
+
+        self.profile = UserProfile.objects.create(avatar='test_avatar.jpg', socials='hotmomsnearyou.com',
+                                                  bio='netflix is overrated', user=self.user)
+
+    def test_string_representation(self):
+        profile = UserProfile(bio='HBO tv app sucks')
+        self.assertEqual(str(profile.bio), profile.bio)
+
+    def test_profile_content(self):
+        self.assertEqual(self.profile.avatar, 'test_avatar.jpg')
+        self.assertEqual(self.profile.socials, 'hotmomsnearyou.com')
+        self.assertEqual(self.profile.bio, 'netflix is overrated')
+        self.assertEqual(self.profile.user.username, 'testuser')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.profile.get_absolute_url(), '/members/user_profile/1/')
+
+    def test_user_profile_view(self):
+        self.client.login(username='testuser', password='Letmeout5!')
+        response = self.client.get(f'/members/user_profile/{self.profile.pk}/')
+        print({self.profile.pk})
+        no_response = self.client.get('/members/user_profile/11111/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'netflix is overrated')
+        self.assertTemplateUsed(response, 'user_profile_details.html')
