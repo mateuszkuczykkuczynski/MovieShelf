b3d53fece3b208057b91b503f7fa583774eb0c94
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-01-21 17:58:21 +0100

    test/members_app_tests_update

--- members/tests.py
+++ members/tests.py
@@ -37,7 +37,6 @@ class SignUpPageTest(TestCase):
 class UserProfileTests(TestCase):
 
     def setUp(self):
-
         self.client = Client()
         self.user = get_user_model().objects.create_user(
             username='testuser',
@@ -46,8 +45,25 @@ class UserProfileTests(TestCase):
             password='Letmeout5!'
         )
 
+        self.user2 = get_user_model().objects.create_user(
+            username='testuser2',
+            email='testuser2@gmail.com',
+            age=421,
+            password='Letmeout55!'
+        )
+
+        self.user3 = get_user_model().objects.create_user(
+            username='testuser3',
+            email='testuser3@gmail.com',
+            age=421,
+            password='Letmeout555!'
+        )
+
         self.profile = UserProfile.objects.create(avatar='test_avatar.jpg', socials='hotmomsnearyou.com',
                                                   bio='netflix is overrated', user=self.user)
+
+        self.profile2 = UserProfile.objects.create(avatar='test_avatar.jpg', socials='hotmomsnearyou.com',
+                                                   bio='netflix is overrated', user=self.user3)
 
     def test_string_representation(self):
         profile = UserProfile(bio='HBO tv app sucks')
@@ -60,14 +76,31 @@ class UserProfileTests(TestCase):
         self.assertEqual(self.profile.user.username, 'testuser')
 
     def test_get_absolute_url(self):
-        self.assertEqual(self.profile.get_absolute_url(), '/members/user_profile/1/')
+        self.assertEqual(self.profile.get_absolute_url(), f'/members/user_profile/{self.profile.pk}/')
 
     def test_user_profile_view(self):
         self.client.login(username='testuser', password='Letmeout5!')
         response = self.client.get(f'/members/user_profile/{self.profile.pk}/')
-        print({self.profile.pk})
         no_response = self.client.get('/members/user_profile/11111/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'netflix is overrated')
         self.assertTemplateUsed(response, 'user_profile_details.html')
+
+    def test_create_user_profile_view(self):
+        self.client.login(username='testuser2', password='Letmeout55!')
+        response = self.client.post(reverse('create_user_profile'),
+                                    {'socials': 'evenhottermomsnearyou.com',
+                                     'bio': 'breaking bad is the best tv sersies ever',
+                                     })
+        self.assertEqual(UserProfile.objects.get(user=self.user2).socials, 'evenhottermomsnearyou.com')
+        self.assertEqual(UserProfile.objects.get(user=self.user2).bio, 'breaking bad is the best tv sersies ever')
+        self.assertEqual(response.status_code, 302)
+
+    def test_update_user_profile_view(self):
+        self.client.login(username='testuser3', password='Letmeout555!')
+        response = self.client.post(reverse('update_user_profile', args='3'),
+                                    {'socials': 'hottestmomsnearyou.com',
+                                     'bio': 'CP2077 was not that bad',
+                                     })
+        self.assertEqual(response.status_code, 302)
