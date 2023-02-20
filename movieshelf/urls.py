3ca8edf64281c1d5c34ea3d83a139f84f72e4b49
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-02-20 23:36:04 +0100

    feat/urls_update

--- movieshelf/urls.py
+++ movieshelf/urls.py
@@ -10,19 +10,23 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import to include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
-from django.views.generic import RedirectView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('members/', include('members.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('scenario/', include('scenario.urls')),
-    # path('', RedirectView.as_view(url='scenario/')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
